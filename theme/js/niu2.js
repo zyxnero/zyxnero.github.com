559085dfa28f98a946719e262a823f3134205058
nero <zyxnero@gmail.com>
2014-02-25 23:46:01 +0800

    测试archives jquery

--- theme/js/niu2.js
+++ theme/js/niu2.js
@@ -10,30 +10,6 @@ window.gFixedTocListOffsetTop = 111;
 window.gFootnotePopoverMaxWidth = 300;
 window.gActiveTocClass = 'niu2-active-toc'
 
-$(document).ready(function() {
-        $("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").attr('rel', 'gallery').fancybox({
-            afterShow: function() {
-                $(".fancybox-title").wrapInner('<div />').show();     
-                $(".fancybox-wrap").hover(function() {
-                $(".fancybox-title").show();
-                 }, function() {
-                $(".fancybox-title").hide();
-                });
-            },  
-
-            openEffect     : 'fade',
-            closeEffect    : 'fade',
-            nextEffect      : 'fade',
-            prevEffect      : 'fade',
-            padding : 0,
-    
-            helpers : {
-                        title : { type : 'over' },
-                        overlay : { locked : false }
-                      }
-    });
-});
-
 
 $(document).ready(function() {
     initGoogleCSEAnimation();
