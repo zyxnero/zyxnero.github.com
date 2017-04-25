46db4bacbd5f7d46d4b7871c94edfa195a0c78bb
nero <zyxnero@gmail.com>
2017-04-25 18:28:29 +0800

    modify gallery num

--- theme/js/picasa.js
+++ theme/js/picasa.js
@@ -1,5 +1,5 @@
 $(function(){
-var name = "zyxnero";var numx = 3;var top= $('#navi').offset().top-20;
+var name = "zyxnero";var numx = 9;var top= $('#navi').offset().top-20;
 $body=(window.opera)?(document.compatMode=="CSS1Compat"?$('html'):$('body')):$('html,body');var href="https://picasaweb.google.com/data/feed/api/user/"+name;$.getJSON(href+"?fields=openSearch:totalResults&alt=json&callback=?",function(c){c=parseInt(c.feed.openSearch$totalResults.$t/numx)+1;for(var b=1;b<=c;){$("#navi").append("<a href='"+b+"'> P "+b+" </a>  ");b++}$("#navi a").eq(1).addClass("current")});
 $("#navi a").live("click",function(){$("#items").fadeOut(500);$(this).addClass("current").siblings().removeClass();var c=$(this).attr("href")-1;$.getJSON(href+"?start-index="+(numx*c+1)+"&max-results="+numx+"&fields=entry(title,gphoto:id,gphoto:numphotos,media:group(media:thumbnail))&alt=json&callback=?",function(b){$("#items").empty();$body.animate({scrollTop:top},400);$(b.feed.entry).each(function(g,d){var f=d.title.$t,e=d.gphoto$id.$t,n=d.gphoto$numphotos.$t,h=d.media$group.media$thumbnail[0].url;$("#items").append("<div class='item'><div class='thumb'><img src='"+
 h+"' id='"+e+"'/><br /><span>"+f+" {"+n+"}</span></div></div>")});$("#items").fadeIn(400)});return false});$("#navi a").click();
